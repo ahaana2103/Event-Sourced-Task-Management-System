c61b7f23cccd9b5e8799f4cd461b46c18bf36557
ajay singh <mrsinghup85@gmail.com>
2026-03-12 15:14:52 +0530

    installations and setUp

---
+++ demoRouting.js
@@ -0,0 +1,9 @@
+const express=require('express');
+const app=express();
+const port =8080;
+app.listen(port,()=>{
+    console.log(`server is listening on port ${port}`);
+});
+app.get('/',(req,res)=>{
+    res.send("hello server ");
+})
