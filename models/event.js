b216e70e9fc79f884dc92cda0cbcd2de54cf1b54
Ahaana Vatsa <ahaanavatsa@gmail.com>
2026-04-14 15:25:44 +0530

    Wprked on immutability

--- models/event.js
+++ models/event.js
@@ -1,11 +1,11 @@
 const mongoose = require("mongoose");
 
 const eventSchema = new mongoose.Schema({
-  eventId: { type: String, required: true },
-  eventType: {type: String, required:true},
-  aggregateId:{type: String, required:true},
-  payload:{type: mongoose.Schema.Types.Mixed, required:true},
-  timestamp: { type: Date, default: Date.now }
+  eventId: { type: String, required: true, immutable: true },
+  eventType: { type: String, required: true, immutable: true },
+  aggregateId: { type: String, required: true, immutable: true },
+  payload: { type: mongoose.Schema.Types.Mixed, required: true, immutable: true },
+  timestamp: { type: Date, default: Date.now, immutable: true }
 });
 
 module.exports = mongoose.model("Event", eventSchema);
